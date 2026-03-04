62ba98d4ce30dc1b56a19153dde22b72d9bc4a2c
paACode <ackermann.pascal@gmail.com>
2026-03-04 14:25:31 +0100

    Should be fixed

--- code/calculator.py
+++ code/calculator.py
@@ -1,4 +1,4 @@
 """A simple calculator module that provides basic operations."""
-def add(a: float, b: float) -> int:
+def add(a: float, b: float) -> float:
     """Add two numbers and return the result."""
     return a + b
